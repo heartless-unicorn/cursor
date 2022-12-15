d38787634453242500bd41327be906896423ab8c
heartless-unicorn <pavlovamofficial@gmail.com>
2022-12-15 16:48:46 +0200

    added last function

--- homework3/script.js
+++ homework3/script.js
@@ -7,6 +7,7 @@ function getMaxDigits(number) {
       result = arr[i];
     }
   }
+  return result;
 }
 // Function 2
 function powerFunc(num, pow) {
@@ -26,25 +27,27 @@ function powerFunc(num, pow) {
   } else if ((pow = 0)) {
     result = 1;
   }
+  return result;
 }
 // Function 3
 function transformName(name) {
   let result = name.toLowerCase().trim();
   let firstLetter = result[0].toUpperCase();
   result = result.replace(result[0], firstLetter);
-  alert(result);
+  return result;
 }
 
 // Function 4
 function afterTax(sum) {
   const tax = 0.195;
   let result = sum - sum * tax;
+  return result;
 }
 
 // function 5
 function getRandomNumber(n, m) {
   let result = Math.round(Math.random() * (m - n) + n);
-  alert(result);
+  return result;
 }
 //function 6
 function getRepeatLetters(letter, str) {
@@ -55,6 +58,7 @@ function getRepeatLetters(letter, str) {
       result += 1;
     }
   }
+  return result;
 }
 // function 7,8
 function convertCurency(str) {
@@ -70,9 +74,12 @@ function convertCurency(str) {
   }
   if (currency != "$" && currency.toLowerCase() === "uah") {
     result = (+result * 0, 027) + "$";
-  } else {
+  } else if (currency == "$") {
     result = (+result * 36, 41) + "UAH";
+  } else {
+    result = "Not the right currency";
   }
+  return result;
 }
 //function 9,10
 function randomPassword(length = 8) {
@@ -82,26 +89,68 @@ function randomPassword(length = 8) {
   }
   let min = +num.slice(0, -1);
   let result = Math.round(Math.random().toFixed(length) * (+num - min) + min);
+  return result;
 }
 //function 11
 function deleteLetters(letter, str) {
   let result = "";
+  str = str.toLowerCase();
   for (l in str) {
     if (str[l] !== letter) {
       result += str[l];
     }
   }
+  return result;
 }
 //function 12
 function isPalyndrom(str) {
-  let result;
   let original = str.replaceAll(" ", "").toLowerCase();
-  console.log(original);
   let reverse = Array.from(original).reverse().join();
   reverse = reverse.replaceAll(",", "");
   if (original == reverse) {
-    result = true;
+    return "Так";
   } else {
-    result = false;
+    return "Ні";
   }
 }
+
+// function 13
+function deleteDuplicateLetter(str) {
+  str = str.toLowerCase().trim().replaceAll(" ", "");
+  let result = str;
+  for (letter in str) {
+    let last = str.lastIndexOf(str[letter]);
+    if (letter != last) {
+      result = result.replaceAll(str[letter], "");
+    }
+  }
+  return result;
+}
+deleteDuplicateLetter("Бісквіт був дуже ніжним");
+
+document.writeln(
+  `Function 1: Найбільша цифра в числі 987 ✨ ${getMaxDigits(987)} ✨ <br/>
+   Function 2: 9 в 6 степені ✨  ${powerFunc(9, 6)} ✨ <br/>
+   Function 3: Відформатоване ім'я ВЛаД ✨${transformName("ВЛаД")}✨ <br/>
+   Function 4: Сума заробітньої плати після оплати податку з 1000 ✨${afterTax(
+     1000
+   )}✨ <br/>
+   Function 5: Випадкове ціле число від 5 до 28 ✨${getRandomNumber(
+     5,
+     28
+   )}✨ <br/>
+   Function 6: Буква "а" в слові "Асталавіста" повторюєся ✨${getRepeatLetters(
+     "а",
+     "Асталавіста"
+   )}✨ разів  <br/>
+   Function 7: 250 гривень це ✨${convertCurency("250UAH")}✨ доларів<br/>
+   Function 8: Рандомний пароль з 5 цифр ✨${randomPassword(5)}✨ <br/>
+   Function 9: "Асталавіста" без "а" ✨${deleteLetters(
+     "а",
+     "Асталавіста"
+   )}✨ <br/>
+   Function 10: "Я несу гусеня" це палідрон? ✨${isPalyndrom(
+     "Я несу гусеня"
+   )}✨  <br/>
+`
+);
