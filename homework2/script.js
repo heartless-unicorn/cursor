6d1dfd35f620b15640db5ad4b2b9bb659cb41407
heartless-unicorn <pavlovamofficial@gmail.com>
2022-12-17 19:07:50 +0200

    fixed numbers are bigger then 0

--- homework2/script.js
+++ homework2/script.js
@@ -1,11 +1,11 @@
 let n = +prompt("Type the first number:");
-while (isNaN(n) || !Number.isInteger(n)) {
-  alert("Please type in a whole number");
+while (isNaN(n) || !Number.isInteger(n) || n <= 0) {
+  alert("Please type in a whole number bigger than 0");
   n = +prompt("Type the first number:");
 }
 let m = +prompt("Type the second number:");
-while (isNaN(m) || !Number.isInteger(m)) {
-  alert("Please type in a whole number");
+while (isNaN(m) || !Number.isInteger(m) || n > m) {
+  alert("Please type in a whole number bigger than the first number");
   m = +prompt("Type the second number:");
 }
 let evenSkip = confirm("Do you want to skip even numbers?");
