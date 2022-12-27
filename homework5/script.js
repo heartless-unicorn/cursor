f609bde1c40f02f129357880208d73a394583b34
heartless-unicorn <pavlovamofficial@gmail.com>
2022-12-27 21:02:17 +0200

    added writeln and fixed bags

--- homework5/script.js
+++ homework5/script.js
@@ -21,15 +21,15 @@ function getModa(...numbers) {
   });
   return result;
 }
-function getAvarage(...numbers) {
+function getAverage(...numbers) {
   let result = 0;
-
   result = numbers.reduce((total, current) => {
     if (!Number.isInteger(current)) {
       return total;
     }
     return total + current;
   }, 0);
+  return result / numbers.length;
 }
 function getMedian(...numbers) {
   numbers = numbers.sort((a, b) => a - b);
@@ -44,7 +44,7 @@ function getMedian(...numbers) {
   return result;
 }
 function filterEvenNumbers(...numbers) {
-  numbers = numbers.filter((value) => value % 2 === 0);
+  numbers = numbers.filter((value) => !(value % 2 === 0));
   return numbers;
 }
 function countPositiveNumbers(...numbers) {
@@ -88,29 +88,128 @@ function divideByThree(str) {
   }
 }
 function generateCombinations(str) {
-  let result = [str];
-  let length = 1;
-  for (let i = 1; i <= str.length; i++) {
-    length = length * i;
-  }
-  let stringArr = Array.from(str);
-  let random = Array(str.length).fill(0);
-  while (length > 1) {
-    for (let i in random) {
-      let randomLetter = Math.round(Math.random() * (stringArr.length - 1));
-      random[i] = stringArr[randomLetter];
-      stringArr.splice(randomLetter, 1);
+  if (str.length < 10) {
+    let result = [str];
+    let length = 1;
+    for (let i = 1; i <= str.length; i++) {
+      length = length * i;
     }
-    let randomString = random.join("");
-    if (result.includes(randomString) || randomString == "") {
+    let stringArr = Array.from(str);
+    let random = Array(str.length).fill(0);
+    while (length > 1) {
+      for (let i in random) {
+        let randomLetter = Math.round(Math.random() * (stringArr.length - 1));
+        random[i] = stringArr[randomLetter];
+        stringArr.splice(randomLetter, 1);
+      }
+      let randomString = random.join("");
+      if (result.includes(randomString) || randomString == "") {
+        stringArr = Array.from(str);
+        continue;
+      }
+      result.push(randomString);
       stringArr = Array.from(str);
-      continue;
+      length--;
     }
-    result.push(randomString);
-    stringArr = Array.from(str);
-    length--;
+    return result;
   }
-  return result;
 }
-
+document.writeln(`
+Функція №1: Масив 15 випадкових чілих чисел від 1 до 100: ✨ ${getRandomArray(
+  15,
+  1,
+  100
+)}✨ </br>
+Функція №2: Мода чисел 6,2,55,11,78,2,55,77,57,87,23,2,56,3,2: ✨ ${getModa(
+  6,
+  2,
+  55,
+  11,
+  78,
+  2,
+  55,
+  77,
+  57,
+  87,
+  23,
+  2,
+  56,
+  3,
+  2
+)}✨ </br>
+Функція №3: Середнє арифметичне чисел 6,2,55,11,78,2,55,77,57,87,23,2,56,3,2: ✨ ${getAverage(
+  6,
+  2,
+  55,
+  11,
+  78,
+  2,
+  55,
+  77,
+  57,
+  87,
+  23,
+  2,
+  56,
+  3,
+  2
+)}✨ </br>
+Функція №4: Модіана чисел 6,2,55,11,78,2,55,77,57,87,23,2,56,3,2: ✨ ${getMedian(
+  6,
+  2,
+  55,
+  11,
+  78,
+  2,
+  55,
+  77,
+  57,
+  87,
+  23,
+  2,
+  56,
+  3,
+  2
+)}✨ </br>
+Функція №5: Непарні числа з 1, 2, 3, 4, 5, 6 : ✨ ${filterEvenNumbers(
+  1,
+  2,
+  3,
+  4,
+  5,
+  6
+)}✨ </br>
+Функція №6: Чисел більше 0 з: 1,
+  -2,
+  3,
+  -4,
+  -5,
+  6  : ✨ ${countPositiveNumbers(1, -2, 3, -4, -5, 6)}✨ </br>
+  Функція №7: З чисел 6,2,55,11,78,2,55,77,57,87,23,2,56,3,2 на п'ять діляться: ✨ ${getDividedByFive(
+    6,
+    2,
+    55,
+    11,
+    78,
+    2,
+    55,
+    77,
+    57,
+    87,
+    23,
+    2,
+    56,
+    3,
+    2
+  )}✨ </br>
+    Функція №8: З цензурою "Are you fucking kidding?" : ✨ ${replaceBadWords(
+      "Are you fucking kidding?"
+    )}✨ </br>
+    Функція №9: Поділено по три букви  "Commander" : ✨ ${divideByThree(
+      "Commander"
+    )}✨ </br>
+    Функція №10: Унікальні комбінації слова  "man" : ✨ ${generateCombinations(
+      "man"
+    )}✨ </br>
+`);
 generateCombinations("live");
