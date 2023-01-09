e54b669d064c9e61028cb43e156b968852c2c8eb
heartless-unicorn <pavlovamofficial@gmail.com>
2023-01-09 23:41:33 +0200

    added functions

--- homework7/script.js
+++ homework7/script.js
@@ -1,4 +1,22 @@
 const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
-getMyTaxes.call(country, salary);
+
+function getMyTaxes(salary) {
+  return this.tax * salary;
+}
+
+function getMiddleTaxes(country) {
+  return this.tax * this.middleSalary;
+}
+function getTotalTaxes(country) {
+  return this.tax * this.middleSalary * this.vacancies;
+}
+function getMySalary(country) {
+  const salary = Math.round(Math.random() * (2000 - 1500) + 1500);
+  const taxes = getMyTaxes.call(country, salary);
+  const result = { salary, taxes, profit: Math.round(salary - taxes) };
+  setInterval(() => console.log(result), 10000);
+}
+console.log(getMyTaxes.call(ukraine, 1000));
+getMySalary(ukraine);
