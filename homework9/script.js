f179fde11bc4acbeb1923f7a45622fbe50a2e30b
heartless-unicorn <pavlovamofficial@gmail.com>
2023-01-15 22:36:15 +0200

    added colors and cjangig colors

--- homework9/script.js
+++ homework9/script.js
@@ -5,6 +5,27 @@ function generateBlocks() {
     for (let i = 0; i < squareAmount; i++) {
       let element = document.createElement("div");
       element.classList.add("block");
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      element.style.backgroundColor = `#${randomColor}`;
+      row.appendChild(element);
+    }
+    row.classList.add("row");
+    document.body.appendChild(row);
+  }
+}
+function generateBlocksInterval() {
+  let squareAmount = 5;
+  for (let i = 0; i < squareAmount; i++) {
+    let row = document.createElement("div");
+    for (let i = 0; i < squareAmount; i++) {
+      let element = document.createElement("div");
+      element.classList.add("block");
+
+      setInterval(() => {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        element.style.backgroundColor = `#${randomColor}`;
+      }, 1000);
+
       row.appendChild(element);
     }
     row.classList.add("row");
@@ -12,4 +33,4 @@ function generateBlocks() {
   }
 }
 
-generateBlocks();
+generateBlocksInterval();
