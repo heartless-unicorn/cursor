f18adfec4fbb9d2839c82732f45d6db7c89d685a
heartless-unicorn <pavlovamofficial@gmail.com>
2023-01-17 16:16:56 +0200

    added style on key press

--- homework10/src/script.js
+++ homework10/src/script.js
@@ -7,16 +7,21 @@ const soundKeys = {
   KeyH: getAudio("name"),
   KeyJ: getAudio("stand"),
 };
-
 document.addEventListener("keydown", (button) => {
   const key = button.code;
   if (key in soundKeys) {
     playSound(soundKeys[key]);
+    let button = Array.from(document.querySelectorAll(`button`)).find(
+      (element) => element.textContent === key.charAt(key.length - 1)
+    );
+    button.classList.add("active");
+    setTimeout(() => button.classList.remove("active"), 1500);
   }
 });
 
 function getAudio(name) {
-  return new Audio(`/sounds/${name}.mp3`);
+  const audio = new Audio(`/media/${name}.mp3`);
+  return audio;
 }
 
 function keyClick(button) {
